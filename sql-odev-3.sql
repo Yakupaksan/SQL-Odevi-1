71fc1a0347ab0a455c2382362765fcb230d3cfcf
Yakupaksan <yakupaksann97@gmail.com>
2022-08-11 22:16:09 +0300

    Update sql-odev-3.sql

--- sql-odev-3.sql
+++ sql-odev-3.sql
@@ -8,4 +8,4 @@ SELECT country FROM country WHERE country LIKE '_____%n'
 SELECT title FROM film WHERE title ILIKE '%t%t%t%t%'
 
 --4
-SELECT title FROM film WHERE title ILIKE '%t%t%t%t%'
+SELECT * FROM film WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99
